562ace927961cf8f34f08b700fde9e1f215f2d3b
Cameron Smith <cameron.ray.smith@gmail.com>
2025-02-26 10:23:16 -0500

    fix(pre-commit): use nixfmt-rfc-style
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- nix/modules/pre-commit.nix
+++ nix/modules/pre-commit.nix
@@ -3,14 +3,22 @@
   imports = [
     (inputs.git-hooks + /flake-module.nix)
   ];
-  perSystem = { config, self', pkgs, lib, ... }: {
-    pre-commit.settings = {
-      hooks = {
-        nixpkgs-fmt.enable = true;
-        ruff.enable = true;
-        ruff-format.enable = true;
-        taplo.enable = true;
+  perSystem =
+    {
+      config,
+      self',
+      pkgs,
+      lib,
+      ...
+    }:
+    {
+      pre-commit.settings = {
+        hooks = {
+          nixfmt-rfc-style.enable = true;
+          ruff.enable = true;
+          ruff-format.enable = true;
+          taplo.enable = true;
+        };
       };
     };
-  };
 }
