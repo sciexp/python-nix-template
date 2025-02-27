13cf56375d3185ff130a24d32bf1322c022fe77b
Cameron Smith <cameron.ray.smith@gmail.com>
2025-02-27 17:18:10 -0500

    fix(package): rename to python-nix-template
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- nix/modules/packages.nix
+++ nix/modules/packages.nix
@@ -18,10 +18,10 @@
     {
       # nix build
       packages = rec {
-        mypackage311 = pythonSets.py311.mkVirtualEnv "mypackage-3.11" baseWorkspace.deps.default;
-        mypackage312 = pythonSets.py312.mkVirtualEnv "mypackage-3.12" baseWorkspace.deps.default;
+        pythonNixTemplate311 = pythonSets.py311.mkVirtualEnv "python-nix-template-3.11" baseWorkspace.deps.default;
+        pythonNixTemplate312 = pythonSets.py312.mkVirtualEnv "python-nix-template-3.12" baseWorkspace.deps.default;
 
-        default = mypackage312;
+        default = pythonNixTemplate312;
       };
 
     };
