3391fa5bd5296d19a11c0bb15cc98be29bd817ed
Cameron Smith <cameron.ray.smith@gmail.com>
2025-03-08 09:03:42 -0500

    fix(python-nix-template): set `__version__` from metadata
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- src/python_nix_template/__init__.py
+++ src/python_nix_template/__init__.py
@@ -1,2 +1,12 @@
+from importlib import metadata
+
+try:
+    __version__ = metadata.version(__package__)
+except metadata.PackageNotFoundError:
+    __version__ = "unknown"
+
+del metadata
+
+
 def main() -> None:
     print("Hello from python-nix-template!")
