ff7a0a06abeb8ac3290a3492da482b874c9580b2
Cameron Smith <cameron.ray.smith@gmail.com>
2026-02-03 10:29:38 -0500

    refactor(nix): move devshell.nix to modules/

---
+++ modules/devshell.nix
@@ -0,0 +1,90 @@
+{
+  inputs,
+  ...
+}:
+{
+  perSystem =
+    {
+      config,
+      self',
+      pkgs,
+      lib,
+      packageWorkspaces,
+      editablePythonSets,
+      pythonVersions,
+      ...
+    }:
+    let
+      # Merge deps from all independent package workspaces, unioning extras lists
+      # for shared dependency names rather than silently dropping via //
+      allDeps = lib.foldlAttrs (
+        acc: _: pkg:
+        lib.zipAttrsWith (_: values: lib.unique (lib.flatten values)) [
+          acc
+          pkg.workspace.deps.all
+        ]
+      ) { } packageWorkspaces;
+
+      mkDevShell =
+        {
+          name,
+          pythonVersion,
+        }:
+        let
+          python = pythonVersions.${pythonVersion};
+          pythonEnv = editablePythonSets.${pythonVersion};
+          virtualenv = pythonEnv.mkVirtualEnv "${name}-dev-env" allDeps;
+        in
+        pkgs.mkShell {
+          inherit name;
+          inputsFrom = [ config.pre-commit.devShell ];
+          packages = with pkgs; [
+            just
+            pixi
+            quarto
+            teller
+            uv
+            yarn-berry
+            virtualenv
+            age
+            gitleaks
+            sops
+            ssh-to-age
+            config.packages.set-git-env
+            # Rust tooling for pnt-cli pyo3 extension
+            cargo
+            rustc
+            clippy
+            cargo-nextest
+            maturin
+          ];
+
+          env = {
+            UV_NO_SYNC = "1";
+            UV_PYTHON = "${virtualenv}/bin/python";
+            UV_PYTHON_DOWNLOADS = "never";
+          };
+
+          shellHook = ''
+            unset PYTHONPATH
+            export REPO_ROOT=$(git rev-parse --show-toplevel)
+            set-git-env
+          '';
+        };
+    in
+    {
+      devShells = rec {
+        pythonNixTemplate311 = mkDevShell {
+          name = "python-nix-template-3.11";
+          pythonVersion = "py311";
+        };
+
+        pythonNixTemplate312 = mkDevShell {
+          name = "python-nix-template-3.12";
+          pythonVersion = "py312";
+        };
+
+        default = pythonNixTemplate312;
+      };
+    };
+}
