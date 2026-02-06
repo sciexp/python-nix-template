d2196b3e1c75fff5e294e0d261e5569771aae125
Cameron Smith <cameron.ray.smith@gmail.com>
2026-02-05 22:29:53 -0500

    feat(nix): add formatting module with prek, treefmt-nix, and gitleaks

---
+++ modules/formatting.nix
@@ -0,0 +1,31 @@
+{ inputs, ... }:
+{
+  imports = [
+    inputs.treefmt-nix.flakeModule
+    inputs.git-hooks.flakeModule
+  ];
+
+  perSystem =
+    { pkgs, ... }:
+    {
+      treefmt = {
+        projectRootFile = "flake.nix";
+        programs.nixfmt.enable = true;
+        programs.ruff-format.enable = true;
+        programs.ruff-check.enable = true;
+        programs.taplo.enable = true;
+      };
+
+      pre-commit.settings = {
+        package = pkgs.prek;
+        hooks.treefmt.enable = true;
+        hooks.gitleaks = {
+          enable = true;
+          name = "gitleaks";
+          entry = "${pkgs.gitleaks}/bin/gitleaks protect --staged --verbose --redact";
+          language = "system";
+          pass_filenames = false;
+        };
+      };
+    };
+}
